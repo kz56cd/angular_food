c32aff7a076fcb21fbe003469f70998bd33f1a1d
kz56cd <kz56cd@gmail.com>
2015-06-08 10:04:50 +0900

    delete unuse comment lines.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -5,9 +5,8 @@ angular.module('app04')
 
     $scope.msg = 'should record all foods if you eat something.';
 
-    $scope.foods = [];
-    var index = 0;
-    // $scope.num = 0;
+    $scope.foods d= [];
+    var index    = 0;
 
     // 追加
     $scope.add = function() {
@@ -61,11 +60,6 @@ angular.module('app04')
 
     // 全て
     $scope.deleteAll = function() {
-        // $scope.foods.forEach(function(food) {
-        //     // food.done = true;
-        //     // $scope.delete(food);
-        // });
-
         $scope.foods.length = 0;
         $scope.foods.push.apply($scope.foods, []);
     };
@@ -82,64 +76,24 @@ angular.module('app04')
                date.getSeconds();
     };
 
-    // $scope.awesomeThings = [
-    //   {
-    //     'title': 'AngularJS',
-    //     'url': 'https://angularjs.org/',
-    //     'description': 'HTML enhanced for web apps!',
-    //     'logo': 'angular.png'
-    //   },
-    //   {
-    //     'title': 'BrowserSync',
-    //     'url': 'http://browsersync.io/',
-    //     'description': 'Time-saving synchronised browser testing.',
-    //     'logo': 'browsersync.png'
-    //   },
-    //   {
-    //     'title': 'GulpJS',
-    //     'url': 'http://gulpjs.com/',
-    //     'description': 'The streaming build system.',
-    //     'logo': 'gulp.png'
-    //   },
-    //   {
-    //     'title': 'Jasmine',
-    //     'url': 'http://jasmine.github.io/',
-    //     'description': 'Behavior-Driven JavaScript.',
-    //     'logo': 'jasmine.png'
-    //   },
-    //   {
-    //     'title': 'Karma',
-    //     'url': 'http://karma-runner.github.io/',
-    //     'description': 'Spectacular Test Runner for JavaScript.',
-    //     'logo': 'karma.png'
-    //   },
-    //   {
-    //     'title': 'Protractor',
-    //     'url': 'https://github.com/angular/protractor',
-    //     'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
-    //     'logo': 'protractor.png'
-    //   },
-    //   {
-    //     'title': 'Angular Material Design',
-    //     'url': 'https://material.angularjs.org/#/',
-    //     'description': 'The Angular reference implementation of the Google\'s Material Design specification.',
-    //     'logo': 'angular-material.png'
-    //   },
-    //   {
-    //     'title': 'Stylus',
-    //     'url': 'http://learnboost.github.io/stylus/',
-    //     'description': 'Stylus is a revolutionary new language, providing an efficient, dynamic, and expressive way to generate CSS. Supporting both an indented syntax and regular CSS style.',
-    //     'logo': 'stylus.png'
-    //   },
-    //   {
-    //     'key': 'jade',
-    //     'title': 'Jade',
-    //     'url': 'http://jade-lang.com/',
-    //     'description': 'Jade is a high performance template engine heavily influenced by Haml and implemented with JavaScript for node.',
-    //     'logo': 'jade.png'
-    //   }
-    // ];
-    // angular.forEach($scope.awesomeThings, function(awesomeThing) {
-    //   awesomeThing.rank = Math.random();
-    // });
+
+// デフォルト例
+// $scope.awesomeThings = [
+//   {
+//     'title': 'AngularJS',
+//     'url': 'https://angularjs.org/',
+//     'description': 'HTML enhanced for web apps!',
+//     'logo': 'angular.png'
+//   },
+//   {
+//     'key': 'jade',
+//     'title': 'Jade',
+//     'url': 'http://jade-lang.com/',
+//     'description': 'Jade is a high performance template engine heavily influenced by Haml and implemented with JavaScript for node.',
+//     'logo': 'jade.png'
+//   }
+// ];
+// angular.forEach($scope.awesomeThings, function(awesomeThing) {
+//   awesomeThing.rank = Math.random();
+// });
   });
